3763e376ba41721d8db3904d0ba361d2ccabcd19
eliasneri <eliasneri@hotmail.com>
2020-10-22 14:26:31 -0300

    Implementação das Classes!

---
+++ src/entities/Product.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Product {
+	
+	private String name;
+	private Double price;
+	
+	public Product() {
+		
+	}
+	
+	public Product(String name, Double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	
+	public String priceTag() {
+		return "$ " + price;
+	}
+}
